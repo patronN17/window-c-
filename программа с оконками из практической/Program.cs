4fd6de2960ebb33e4f30ec4bb5ac31cf1f29698d
Patron <Patron@LAPTOP-VIEAF2J0>
2023-01-12 02:26:46 +0300

    Добавьте файлы проекта.

---
+++ программа с оконками из практической/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace программа_с_оконками_из_практической
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = MessageBox.Show("Леон топ ? ","Siiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii",MessageBoxButtons.YesNo,MessageBoxIcon.Exclamation);
+            if (key == DialogResult.Yes)
+                MessageBox.Show("леон лучше ");
+            else
+                MessageBox.Show("ошибся с выбором ботик)))");
+
+        }
+    }
+}
